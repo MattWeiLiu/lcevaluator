08539d6b65fae19f7f92441b02f1b22d590e9ffe
Matthew Liu <matthew.liu@mile.cloud>
2019-03-28 03:11:24 +0000

    Update augmentation.py

--- package/augmentation.py
+++ package/augmentation.py
@@ -168,7 +168,8 @@ def denoiseImage(image):
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
     connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
     # using RETR_EXTERNAL instead of RETR_CCOMP
-    im2, contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    # im2, contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     mask = np.zeros(bw.shape, dtype=np.uint8)
     for idx in range(len(contours)):
